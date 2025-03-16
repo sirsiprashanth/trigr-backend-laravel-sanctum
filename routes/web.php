cfa4c142e8ed8affc645027f27ef0ea436d37194
sirsiprashanth <prashanthsirsi@gmail.com>
2025-03-16 11:20:44 +0530

    Add new HTML files, assets, and update routing for TrigrWireFrame2 project
    - Introduced new HTML files: choose.html, Final.html, index.html, PopUp.html, and Subs1-4.html.
    - Added Bootstrap CSS and JS files for styling and functionality.
    - Included various image assets, icons, and fonts to enhance the visual design.
    - Updated web.php to redirect the root URL to the new index.html for improved navigation.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect('/TrigrWireFrame2/index.html');
+    return redirect('/app/index.html');
 });
 
 Route::get('/dashboard', function () {
